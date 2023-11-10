33ea6505cf2d6d6da92cca19f23fa5db46ce1ca6
Esmatullah <esmatqadeemi12@gmail.com>
2023-11-10 18:40:21 +0100

    complete the challenge

--- decode_morse.rb
+++ decode_morse.rb
@@ -40,3 +40,25 @@ MORSE_SYMBOL = {
 def decode_char(morse_char)
   MORSE_SYMBOL[morse_char]
 end
+
+def decode_word(morse_words)
+  @decode_word = ''
+  charachters = morse_words.split
+  charachters.each do |n|
+    @decode_word += decode_char(n)
+  end
+  @decode_word
+end
+  
+def decode_message(morse_message)
+  @decode_message = ''
+  message_words = morse_message.split('   ')
+  message_words.each_with_index do |word, index|
+    @decode_message += decode_word(word)
+    @decode_message += ' ' unless index == message_words.size - 1
+  end
+print @decode_message
+end
+  
+decode_message('.-   -... --- -..-   ..-. ..- .-.. .-..   --- ..-.   .-. ..- -... .. . ...')
+# A BOX FULL OF RUBIES
